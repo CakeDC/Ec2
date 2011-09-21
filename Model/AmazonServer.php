b1f07dfd72bdffb2f03bb3e32454a3a27dc76598
Graham Weldon <graham@grahamweldon.com>
2011-09-22 00:21:55 +1000

    Added stop() call and a response parse for amazon responses.

--- Model/AmazonServer.php
+++ Model/AmazonServer.php
@@ -1,12 +1,72 @@
 <?php
-
 App::import('Vendor', 'CFRuntime', null, null, 'AWSSDKforPHP/sdk.class.php')
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+App::uses('CakeDocument', 'MongoCake.Model');
 
-class AmazonServer extends AppModel {
+/** @ODM\Document */
+class AmazonServer extends CakeDocument {
 
-	public function __call($method, $args) {
-		$ec2 = new AmazonEC2();
-		return $ec2->$method($args);
+/**
+ * Server Id
+ *
+ * @ODM\Id
+ * @var string
+ */
+	private $id;
+
+/**
+ * Amazon assigned instance ID
+ *
+ * @ODM\String
+ * @var string
+ */
+	public $instanceId;
+
+/**
+ * Public IP Address (DNS)
+ *
+ * @ODM\String
+ * @var string
+ */
+	public $ipAddress;
+
+/**
+ * Creation date and time
+ *
+ * @ODM\Date
+ * @var DateTime
+ */
+	public $created;
+
+/**
+ * Modified date and time
+ *
+ * @ODM\Date
+ * @var DateTime
+ */
+	public $modified;
+
+/**
+ * Pause the instance (Only works for EBS backed instances)
+ *
+ * @return boolean True if the operation was a success
+ */
+	public function stop() {
+		if (!$this->instanceId) {
+			throw new EC2_Exception('AmazonServer has no instance Id');
+		}
+		$ec2 = new AmazonEc2();
+		$response = $ec2->stop_instances($this->instanceId);
+		return $this->_amazonResponseOK($response);
 	}
 
+/**
+ * Determine if the response from Amazon was "Ok"
+ *
+ * @param CFResponse $response Response object
+ * @return boolean True if success
+ */
+	protected function _amazonResponseOk(CFResponse $response) {
+		return $response->isOK();
+	}
 }
