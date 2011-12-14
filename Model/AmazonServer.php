1390904c749eb9d6255fc4f04d9cdcec773c1864
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2011-12-13 20:37:17 -0430

    Adding license at the top of every file

--- Model/AmazonServer.php
+++ Model/AmazonServer.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Copyright 2011, Cake Development Corporation (http://cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2011, Cake Development Corporation (http://cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 App::uses('CakeDocument', 'MongoCake.Model');
 
